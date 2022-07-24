33efe7603b1c9c86e6b65099503b65725e7aa836
Titus Quantum <rajrakshit212@gmail.com>
2022-07-24 11:19:40 +0530

    added a overloaded series program

---
+++ Series/overload_series.java
@@ -0,0 +1,21 @@
+class overload_series {
+    double sum=0;
+    double overload_series(double n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            sum+=1/i;
+        }
+        return sum;
+    }
+    double overload_series(double a, double n)
+    {
+        int c=1;
+        for(int i=1;i<=n;i++)
+        {
+            sum+=c/Math.pow(a,c+1);
+            c+=3;
+        }
+        return sum;
+    }
+}
