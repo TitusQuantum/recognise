bef3ec0f0bedd878b2e32424e9380717f305463e
Titus Quantum <rajrakshit212@gmail.com>
2022-08-11 20:03:37 +0530

    fixed errors in automorphic program

--- project_program.java
+++ project_program.java
@@ -168,18 +168,16 @@ class Project{
     void Automorphic()
     {
         System.out.print("Enter the number : ");
-        int n=sc.nextInt(), c=0, div=0, rem=0, dup=n;
-        n=(int)Math.pow(n,2);
+        int n=sc.nextInt(), c=0, d=0, dup=n;
         while(n>0)
         {
             c++;
             n=n/10;
         }
-        div = (int)Math.pow(10,c);
-        rem = n%div;
-        if(rem==dup)
+        d = (int)Math.pow(10,c);
+        if((dup*dup)%d==dup)
             System.out.println("Entered number is a automorphic number");
-        else 
+        else
             System.out.println("Entered number is not a automorphic number");
     }
     void Spy()
